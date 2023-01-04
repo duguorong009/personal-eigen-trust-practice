5382ff3bf33a09ba6bc7c3a8af6097642ab5583a
duguorong <dudurong009@gmail.com>
2023-01-04 07:32:26 -0800

    Add the function of normalizing the local trust values

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,10 @@ fn main() {
     // local trust values
     let s_i_j = mat_sub(sat_data, unsat_data);
 
-    println!("Local trust values:: {s_i_j:?}");
+    // normalized local trust values
+    let c_i_j = normalize(s_i_j);
+
+    println!("Normalized local trust values:: {c_i_j:?}");
 }
 
 fn rng_tr_data(m: usize) -> Vec<Vec<u8>> {
@@ -36,6 +39,24 @@ fn mat_sub(a: Vec<Vec<u8>>, b: Vec<Vec<u8>>) -> Vec<Vec<i16>> {
     for i in 0..m {
         for j in 0..m {
             res[i][j] = a[i][j] as i16 - b[i][j] as i16;
+        }
+    }
+
+    res
+}
+
+fn normalize(s: Vec<Vec<i16>>) -> Vec<Vec<f64>> {
+    let m = s.len();
+    let mut res: Vec<Vec<f64>> = vec![vec![0.0; m]; m];
+
+    for i in 0..m {
+        for j in 0..m {
+            let sum = s[i].iter().map(|s_i_j| s_i_j.max(&0)).sum::<i16>();
+            res[i][j] = if sum == 0 {
+                1.0 / m as f64 // TODO: should be the trust value of initial trusted peers - p_i
+            } else {
+                s[i][j].max(0) as f64 / sum as f64
+            };
         }
     }
 
