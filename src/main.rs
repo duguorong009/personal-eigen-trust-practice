0f39dd90fe617b35ab951ff636c9b59b86af39ec
duguorong <dudurong009@gmail.com>
2023-01-05 01:23:21 -0800

    Some cleaning

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,19 @@
-mod utils;
-use nalgebra::{ArrayStorage, EuclideanNorm, Matrix, Norm, SMatrix, U1, U10};
+use nalgebra::{ArrayStorage, Matrix, SMatrix, U10};
 
+mod utils;
 use crate::utils::csv::{read_from_csv_file, write_to_csv_file};
 use crate::utils::random::gen_random_downloads_data;
 
 const M: usize = 10; // count of peers
+const SAT_DATA_FILE: &str = "sat_downloads.csv"; // file path of satisfactory downloads counts data
+const UNSAT_DATA_FILE: &str = "unsat_downloads.csv"; // file path of unsatisfactory downloads counts data
 
 fn main() {
-    println!("Hello, world!");
-
-    let sat_data_file_path: String = "sat_downloads.csv".to_string();
-    let unsat_data_file_path: String = "unsat_downloads.csv".to_string();
-
-    // // Generate random data & write to files for future use
-    // let sat_downloads = gen_random_downloads_data(m);
-    // write_to_csv_file(&sat_data_file_path, m, &sat_downloads).unwrap();
-
-    // let unsat_downloads = gen_random_downloads_data(m);
-    // write_to_csv_file(&unsat_data_file_path, m, &unsat_downloads).unwrap();
+    // generate_sim_data();
 
     // Read downloads data for eigentrust computation
-    let sat_data = read_from_csv_file(&sat_data_file_path).unwrap();
-    let unsat_data = read_from_csv_file(&unsat_data_file_path).unwrap();
+    let sat_data = read_from_csv_file(SAT_DATA_FILE).unwrap();
+    let unsat_data = read_from_csv_file(UNSAT_DATA_FILE).unwrap();
 
     // Convert the downloads data to matrix for operations
     let sat_mat: SMatrix<f64, M, M> =
@@ -29,15 +21,26 @@ fn main() {
     let unsat_mat: SMatrix<f64, M, M> =
         SMatrix::from_vec(unsat_data.concat().into_iter().map(|v| v as f64).collect());
 
-    // // local trust values : s_i_j
+    // local trust values : s(i, j) = sat(i, j) - unsat(i, j)
     let s: SMatrix<f64, M, M> = sat_mat - unsat_mat;
 
-    // // normalized local trust values : c_i_j
+    // normalized local trust values : c(i, j)
     let c: SMatrix<f64, M, M> = custom_normalize(s);
 
-    // // transposed local trust values: C_T
-    // let c_t = c.transpose();
+    basic_eigentrust_algo(c);
+}
 
+/// Generate random data for simulation & save to files for future use
+fn generate_sim_data() {
+    let sat_downloads = gen_random_downloads_data(M);
+    write_to_csv_file(&SAT_DATA_FILE, M, &sat_downloads).unwrap();
+
+    let unsat_downloads = gen_random_downloads_data(M);
+    write_to_csv_file(&UNSAT_DATA_FILE, M, &unsat_downloads).unwrap();
+}
+
+/// Simple centralized trust value computing algorithm (basic EigenTrust algorithm)
+fn basic_eigentrust_algo(c: SMatrix<f64, M, M>) {
     let mut t_i = c.clone(); // t_0 = c_0
     let mut t_i_next: Matrix<f64, U10, U10, ArrayStorage<f64, M, M>> = Matrix::default();
 
