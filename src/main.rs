6a8cc672a295ada0c75fc0ad37c2b563fb078d7b
duguorong <dudurong009@gmail.com>
2023-01-05 10:51:15 -0800

    Add the basic EigenTrust algorithm practice

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use nalgebra::{ArrayStorage, Matrix, SMatrix, U10};
+use nalgebra::{ArrayStorage, Matrix, SMatrix, U1, U10};
 
 mod utils;
 use crate::utils::csv::{read_from_csv_file, write_to_csv_file};
@@ -27,22 +27,22 @@ fn main() {
     // normalized local trust values : c(i, j)
     let c: SMatrix<f64, M, M> = custom_normalize(s);
 
-    simple_non_distributed_eigen_trust(c);
-}
+    let peer = 1;
+    let a = 0.4; // constant less than 1
+    let p: Matrix<f64, U10, U1, ArrayStorage<f64, M, 1>> =
+        SMatrix::from_vec(vec![0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0]);
 
-/// Generate random data for simulation & save to files for future use
-fn generate_sim_data() {
-    let sat_downloads = gen_random_downloads_data(M);
-    write_to_csv_file(&SAT_DATA_FILE, M, &sat_downloads).unwrap();
+    simple_non_distributed_eigen_trust(c, peer);
 
-    let unsat_downloads = gen_random_downloads_data(M);
-    write_to_csv_file(&UNSAT_DATA_FILE, M, &unsat_downloads).unwrap();
+    basic_eigen_trust(c, peer, a, p);
 }
 
 /// Simple centralized trust value computing algorithm (Algorithm 1. Simple non-distributed EigenTrust algorithm)
-fn simple_non_distributed_eigen_trust(c: SMatrix<f64, M, M>) {
-    let mut t_i = c.clone(); // t_0 = c_0
-    let mut t_i_next: Matrix<f64, U10, U10, ArrayStorage<f64, M, M>> = Matrix::default();
+fn simple_non_distributed_eigen_trust(c: SMatrix<f64, M, M>, peer: usize) {
+    println!("Simple non-distributed EigenTrust algorithm");
+
+    let mut t_i = c.row(peer).transpose(); // t_0 = c_0
+    let mut t_i_next: Matrix<f64, U10, U1, ArrayStorage<f64, M, 1>> = Matrix::default();
 
     let mut n = 0;
     let mut sig = f64::MAX;
@@ -60,6 +60,42 @@ fn simple_non_distributed_eigen_trust(c: SMatrix<f64, M, M>) {
 
     println!("t_i: {t_i:?}");
     println!("after {n} iterations!");
+}
+
+/// Basice EigenTrust algorithm (Algorithm 2.)
+fn basic_eigen_trust(c: SMatrix<f64, M, M>, _peer: usize, a: f64, p: SMatrix<f64, M, 1>) {
+    println!("Basice EigenTrust algorithm");
+
+    let mut t_i = p; // t_0 = p
+    let mut t_i_next: Matrix<f64, U10, U1, ArrayStorage<f64, M, 1>> = Matrix::default();
+
+    let mut n = 0;
+    let mut sig = f64::MAX;
+    let err = 0.05;
+
+    while sig > err {
+        c.tr_mul_to(&t_i, &mut t_i_next);
+
+        t_i_next = (1.0 - a) * t_i_next + a * p;
+
+        sig = (t_i_next - t_i).norm();
+
+        t_i = t_i_next;
+
+        n += 1;
+    }
+
+    println!("t_i: {t_i:?}");
+    println!("after {n} iterations!");
+}
+
+/// Generate random data for simulation & save to files for future use
+fn generate_sim_data() {
+    let sat_downloads = gen_random_downloads_data(M);
+    write_to_csv_file(&SAT_DATA_FILE, M, &sat_downloads).unwrap();
+
+    let unsat_downloads = gen_random_downloads_data(M);
+    write_to_csv_file(&UNSAT_DATA_FILE, M, &unsat_downloads).unwrap();
 }
 
 /// Normalize the matrix of local trust values s(i, j) by using the
